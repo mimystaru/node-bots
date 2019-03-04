5611682ff1e8c2f700a0984feb554f76bbb239db
mimystaru <skittlesskitty@gmail.com>
2019-03-04 00:59:16 -0500

    Auto-Commit

--- chocola.js
+++ chocola.js
@@ -2,6 +2,8 @@ const Discord = require ("discord.js"); // discord client
 const client = new Discord.Client(); // discord client
 const settings = require('./settings.json')
 
+
+
 client.on("ready", () => {
   console.log("Chocola is here nya!")
   client.user.setStatus("online")
--- up.s
+++ up.s
@@ -1,11 +1,10 @@
 #!/bin/bash
-node vanilla.js & disown;
-node chocola.js & disown;
+#node vanilla.js & disown;
+#node chocola.js & disown;
 #node have-a-gif.js & disown;
 #node lunarmoth.js & disown;
 #node coconutdreams.js & disown;
 #node maple.js & disown;
 #node momiji-supports-chu.js & disown;
 #node rawr-uwu.js & disown;
-#npm run start
-
+npm run vanilla & npm run chocola
