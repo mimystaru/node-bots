325f4d9038e6845acaf832489542cd82f4db5a3d
mimystaru <skittlesskitty@gmail.com>
2019-03-20 22:38:20 -0400

    update

--- chocola.js
+++ chocola.js
@@ -3,6 +3,16 @@ const client = new Discord.Client(); // discord client
 const settings = require('./settings.json')
 
 
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (message === null) return;
+  if (message.isMentioned(client.user.id)) {
+    message.reply("Chocala is here, yes Hii!!")
+  }
+  if (message.content.startsWith("c info")) {
+    message.reply(`hiya , Chocola here! I am a NSFW image bot, please understand that i am still in development.NYA.\ntype \`c pic\` to get a picture!`)
+  }
+});
 
 client.on("ready", () => {
   console.log("Chocola is here nya!")
--- vanilla.js
+++ vanilla.js
@@ -1,6 +1,14 @@
 const Discord = require ("discord.js"); // discord client
 const client = new Discord.Client(); // discord client
 const settings = require('./settings.json')
+var fs = require("fs")
+var text = fs.readFileSync("./vanilla-pictures.txt").toString('utf-8')
+var PicData = text.split('\n');
+
+function Rand(data) {
+  // where data is the array
+  return data[Math.floor(Math.random() * data.length)]
+}
 
 client.on("ready", () => {
   console.log("Vanilla is here nya!")
@@ -13,8 +21,12 @@ client.on("message", message => {
   if (message.isMentioned(client.user.id)) {
     message.reply("I am online!")
   }
-
-
+  if (message.content.startsWith("v info")) {
+    message.reply(`hello, pleased to meet you. I am vanilla, an image bot. please be patient with me as i am still in development.nya.\nType \`v pic\` to get a picture!`)
+  }
+  if (message.content.startsWith("v pic")) {
+    message.reply(Rand(PicData));
+  }
 
 })
 
